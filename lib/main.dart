0bbfa019e453d72741efe5ca663cd0663159b404
tricker3333 <tricker3333@gmail.com>
2023-11-25 15:39:59 +0530

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
